57a65653777f1e8b7b253e7224a91e8641b3904b
Jamf02 <jorge.martinez@ucb.edu.bo>
2023-10-29 19:35:20 -0400

    navbar y modulos vacios historial y tratamiento

---
+++ src/app/modules/histClinica/create/create.component.ts
@@ -0,0 +1,37 @@
+/*import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { PacienteService } from '../histClinica.service';
+
+@Component({
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss'],
+})
+export class CreateComponent {
+  histClinicaData: any = {
+    nombre: '',
+    apellido: '',
+    celular: '',
+    direccion: '',
+    documento_identidad: '',
+    sexo: 'Masculino', // Valor predeterminado
+    fecha_nacimiento: '',
+    enfermedad_base: '',
+  };
+
+  constructor(private pacienteService: PacienteService, private router: Router) {}
+
+  onSubmit() {
+    // Llamar al servicio para crear un paciente con this.pacienteData
+    this.pacienteService.createPaciente(this.pacienteData).subscribe((response: any) => {
+      // Manejar la respuesta del servicio (por ejemplo, redireccionar o mostrar un mensaje)
+      this.router.navigate(['/paciente/list']); // Redirigir a la lista de pacientes
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/paciente/list']); // Volver atrás sin guardar cambios
+  }
+}
+
+*/
