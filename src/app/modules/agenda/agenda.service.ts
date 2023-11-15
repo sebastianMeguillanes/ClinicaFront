92968e43d95aa76950d182f34a5a81dcd762cefe
Jamf02 <jorge.martinez@ucb.edu.bo>
2023-11-15 17:56:57 -0400

    integracion tablas de agenda temprana

--- src/app/modules/agenda/agenda.service.ts
+++ src/app/modules/agenda/agenda.service.ts
@@ -7,6 +7,12 @@ import { HttpClient } from '@angular/common/http';
 export class AgendaService {
  private apiUrl = 'https://clinicaback.dev.404.codes/api/v1/agenda/';
 
+
+
+
+
+
+ 
   constructor(private http: HttpClient) {}
 
 }
