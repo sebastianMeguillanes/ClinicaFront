bcb78707f391e20eae5f5b583411db66e94b8eaf
Jamf02 <jorge.martinez@ucb.edu.bo>
2023-11-20 09:01:35 -0400

    cambios app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, NavigationStart, Router, Event as NavigationEvent } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { CreateComponent } from './create/create.component';
-
 import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
